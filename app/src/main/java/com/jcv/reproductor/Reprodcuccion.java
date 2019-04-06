1f6ac53a4d5f0f88859d20bd709fd2b9d6a1a378
jcv <nahoj1992@gmail.com>
2019-04-06 10:09:03 -0500

    version 1.0

---
+++ app/src/main/java/com/jcv/reproductor/Reprodcuccion.java
@@ -0,0 +1,13 @@
+package com.jcv.reproductor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reprodcuccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reprodcuccion);
+    }
+}
